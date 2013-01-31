fcfe2e87c7c80096911a098cc9b1b8d677f45659
Zachary Allaun <zach.allaun@gmail.com>
2013-01-30 22:53:59 -0500

    Dispatch creates new instance and executes command

--- lib/hs/command.rb
+++ lib/hs/command.rb
@@ -1,10 +1,33 @@
 module HS
 
   class Command
-    def self.dispatch(c)
-      Proc.new do |global_options, options, args|
-        puts "#{c.name} command ran. options: #{options}\nargs: #{args}"
+    def self.dispatch(command)
+      Proc.new do |global_opts, opts, args|
+        self.new(global_opts, opts, args).execute(command)
       end
+    end
+
+    def initialize(global_opts, opts, args)
+      @global_opts = global_opts
+      @opts = opts
+      @args = args
+    end
+
+    def execute(command)
+      # validations, etc.
+      send(command.name)
+    end
+
+    def request
+      puts "requesting"
+    end
+
+    def review
+      puts "reviewing"
+    end
+
+    def submit
+      puts "submitting"
     end
   end
 
